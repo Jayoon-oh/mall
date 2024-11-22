c47cb366221e6eeeede7a1a434971410907cd361
GD <GD@DESKTOP-KVDOT51>
2024-11-22 11:18:05 +0900

    vo 업데이트

---
+++ src/main/java/com/example/mall/vo/Orders.java
@@ -0,0 +1,16 @@
+package com.example.mall.vo;
+
+import lombok.Data;
+
+@Data
+public class Orders {
+	private Integer ordersNo;
+	private Integer goodsNo;
+	private Integer ordersAmount;
+	private Integer paymentNo;
+	private String updateDate;
+	private String createDate;
+	
+	
+	
+}
---
+++ src/main/java/com/example/mall/vo/Payment.java
@@ -0,0 +1,13 @@
+package com.example.mall.vo;
+
+import lombok.Data;
+
+@Data
+public class Payment {
+	private Integer paymentNo;
+	private Integer addressNo;
+	private Integer paymentPrice;
+	private String paymentMethod; 
+	private String paymentState;
+	
+}
